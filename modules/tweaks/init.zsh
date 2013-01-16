7cea20b432e823a2886edfa9868123acb455cc44
Pierre <pierre@glei.ch>
2013-01-16 19:48:08 +0100

    some more aliases

--- modules/tweaks/init.zsh
+++ modules/tweaks/init.zsh
@@ -2,3 +2,7 @@
   alias lf='ls -Gl | grep ^d' #Only list directories
   alias lsd='ls -Gal | grep ^d' #Only list directories, including hidden ones
   alias tm='ps -ef | grep'
+  alias confcat="sed -e 's/[#;].*//;/^\s*$/d' "$@""
+  alias whead="curl --head $1"
+  alias gitfukit='git clean -d -x -f; git reset --hard'
+  alias servethis="python -c 'import SimpleHTTPServer; SimpleHTTPServer.test()'"
