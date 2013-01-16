b59cf7dc63a2444a4aa9fa36922543dc2a2456ec
Pierre <pierre@glei.ch>
2013-01-16 14:36:21 +0100

    custom tewaks
    custom tweaks with some aliases

---
+++ modules/tweaks/init.zsh
@@ -0,0 +1,4 @@
+  alias lg='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
+  alias lf='ls -Gl | grep ^d' #Only list directories
+  alias lsd='ls -Gal | grep ^d' #Only list directories, including hidden ones
+  alias tm='ps -ef | grep'
